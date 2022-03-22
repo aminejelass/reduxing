4a73bef0d2c591c7ae172489bd3ac96443c37234
=aminejelass <=aminejelassi95@gmail.com>
2022-03-22 15:23:40 +0100

    redux basics

--- reduxing/src/App.js
+++ reduxing/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
+  var arr = [1,1,1]
+  var i = 0 
+  var khalil =()=>{ i++ ; alert("khalil is a big gay"+i) }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+ 
     </div>
   );
 }
--- reduxing/src/index.js
+++ reduxing/src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createStore } from "redux";
 
+//store globalized store
+//action increment
+const increment = () => {
+  return { type: "INCREMENT" };
+};
+const decriment = ()=>{ 
+  return {type : "DECREMENT"}
+}
+//reducer
+const counter = (state = 0, action)=>{
+   switch(action.type)
+   {
+     case "INCREMENT" : 
+           return state+1 ;
+     case "DECREMENT" : 
+           return state-1 ; 
+   }
+}
+
+let store = createStore(counter)
+store.subscribe(()=>console.log(store.getState(),"hhhhhhhhhhhhhhhhhhhhhhhhhh"))
+//dispatsh
+store.dispatch(increment())
+store.dispatch(increment())
+store.dispatch(increment())
+store.dispatch(increment())
+store.dispatch(increment())
+store.dispatch(increment())
+store.dispatch(decriment())
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
